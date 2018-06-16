eab1208526442f47ec62060434dfc9f8f6df08c5
Kevin Schuchard <schuchard.kevin@gmail.com>
2018-06-15 22:38:56 -0600

    feat(setup):  test schematic - fix tsconfig - default export

--- src/prettier/index.ts
+++ src/prettier/index.ts
@@ -1,7 +1,8 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
-export function prettier(options: any): Rule {
+export default function(options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    tree.create(options.name || 'hello', 'world');
     return tree;
   };
 }
