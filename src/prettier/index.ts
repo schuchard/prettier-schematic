060b2df6ed26ca4f81a161b86db013391fbf3228
Kevin Schuchard <schuchard.kevin@gmail.com>
2018-06-15 21:50:15 -0600

    update package.json

--- src/prettier/index.ts
+++ src/prettier/index.ts
@@ -1,8 +1,5 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
-
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
 export function prettier(options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     return tree;
