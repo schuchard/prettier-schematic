1d00864966a8051647e2fd883f34411ef306624f
Kevin Schuchard <schuchard.kevin@gmail.com>
2018-06-20 21:45:42 -0600

    chore(prettier): add file to version control

---
+++ src/prettier/files/prettier.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  printWidth: <%= printWidth %>,
+  tabWidth: <%= tabWidth %>,
+  useTabs: <%= useTabs %>,
+  semi: <%= semi %>,
+  singleQuote: <%= singleQuote %>,
+  trailingComma: <%= trailingComma %>,
+  bracketSpacing: <%= bracketSpacing %>,
+  jsxBracketSameLine: <%= jsxBracketSameLine %>,
+  arrowParens: <%= arrowParens %>,
+  rangeStart: <%= rangeStart %>,
+  rangeEnd: <%= rangeEnd %>,
+  requirePragma: <%= requirePragma %>,
+  insertPragma: <%= insertPragma %>,
+  proseWrap: <%= proseWrap %>,
+}
