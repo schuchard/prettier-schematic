19fba2a2654c121b3d0a8b27e96add6bfdd03cac
kevin schuchard <schuchard.kevin@gmail.com>
2019-03-08 23:34:40 -0700

    ng new 7.3.4

--- sandbox/README.md
+++ sandbox/README.md
@@ -1,6 +1,6 @@
 # Sandbox
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.4.
 
 ## Development server
 
--- sandbox/src/app/app.component.spec.ts
+++ sandbox/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -12,20 +13,23 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'sb'`, async(() => {
+  });
+
+  it(`should have as title 'sandbox'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('sb');
-  }));
-  it('should render title in a h1 tag', async(() => {
+    expect(app.title).toEqual('sandbox');
+  });
+
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to sb!');
-  }));
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to sandbox!');
+  });
 });
