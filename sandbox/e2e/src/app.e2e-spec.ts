910195745f75df40717cbe87d7b9b4c0d87830e1
Kevin Schuchard <schuchard.kevin@gmail.com>
2020-11-28 22:42:41 -0600

    test(sandbox): update to ng 11

--- sandbox/e2e/src/app.e2e-spec.ts
+++ sandbox/e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to sandbox!');
+  it('should display welcome message', async () => {
+    await page.navigateTo();
+    expect(await page.getTitleText()).toEqual('sandbox app is running!');
   });
 
   afterEach(async () => {
