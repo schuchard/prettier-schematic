abc2cf6e26ef637d8d99d0fddf2cac67160bdbb2
Kevin Schuchard <schuchard.kevin@gmail.com>
2020-12-02 20:58:54 -0600

    style: spacing

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-	printWidth: 100,
-	tabWidth: 2,
-	useTabs: true,
-	semi: true,
-	singleQuote: true,
-	trailingComma: 'es5',
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
 };
